401eda42119fbcbe5d40a6c9b144803b73a5cf23
Calvin D. Woo <calvin.d.woo@gmail.com>
2019-01-02 02:58:07 -0500

    Started adding epsilon-greedy step to Q-learning algorithm for frozenlake.

--- frozen-lake.py
+++ frozen-lake.py
@@ -20,4 +20,28 @@ discount_factor = 0.8
 num_of_steps = 100
 learning_rate = 0.6
 
-# Our policy will be epsilon-greedy, i.e. we will 
+# Our policy will be epsilon-greedy, i.e. we will pick a uniformly random number and compare it to a
+# progressively shrinking epsilon, which dictates whether we take a greedy policy or a random-choice policy.
+epsilon = 1.0
+min_epsilon = 0.01
+decay_rate = 0.05
+
+"""
+Now we implement the Q-learning algorithm. As the Q-table is already initialized, we proceed in 3 steps:
+1) Select epsilon-greedy action using current Q-table estimates.
+2) Proceed to next state using chosen action and collect reward.
+3) Update Q-table using the Bellman equation update rule. Repeat.
+"""
+rewards = []
+for episode in range(epochs):
+    state = env.reset()
+    done = False
+    total_rewards = 0
+
+    for step in range(num_of_steps):
+        # Select epsilon-greedy action
+        prob = random.uniform(0, 1)
+        if prob > epsilon:
+            action = np.argmax(qtable[state,:])
+        else:
+            
